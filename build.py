41455e53516c6ff17158a6c4eeb407735ac78dac
Rally <gg64mail@protonmail.com>
2026-02-05 12:32:23 +0100

    Squashed commit of the following:
    commit 14b1dc4a33c5574d5ffde94904b1629d986afe17
    Author: Rally <gg64mail@protonmail.com>
    Date:   Thu Feb 5 12:28:01 2026 +0100
    
        Added the ssh checker and adder! Users can now add a ssh server to their known_hosts with a gui, as well as check if their currently saved connections are online! They can even send them a command!
    
    commit df249d941c152fc6d80133f96faf2c9f7f2d2da3
    Author: Rally <gg64mail@protonmail.com>
    Date:   Sat Jan 31 18:57:40 2026 +0100
    
        created a ssh checker rather then picker, feature will stay though may be transformed into a ssh adder. shh picker next to dev
    
    commit 4bb5c003400e017412a409a90c9989511597abe0
    Author: Rally <gg64mail@protonmail.com>
    Date:   Fri Jan 30 10:39:16 2026 +0100
    
        debugging is operational, but connecting to network is still an issue [SSH Error: No authentication methods available]
    
    commit 2f772bcbf776e0a7dd11966275f4aabb99320f8a
    Author: Rally <gg64mail@protonmail.com>
    Date:   Tue Jan 27 16:38:08 2026 +0100
    
        Added the UI with scaling, work on the backend is to come once more familiar with paramiko.

--- build.py
+++ build.py
@@ -5,10 +5,11 @@ import os
 import platform
 import subprocess
 import re
+import paramiko
 from PySide6 import QtCore,QtWidgets,QtGui
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtGui import QPixmap, QAction, QWindow, QScreen
-from PySide6.QtCore import Qt, QSize, QObject, QSettings, Slot
+from PySide6.QtWidgets import QApplication, QWidget, QStyle
+from PySide6.QtGui import QPixmap, QAction, QWindow, QScreen, QIcon
+from PySide6.QtCore import Qt, QSize, QObject, QSettings
 
 app=QtWidgets.QApplication([])
 codecwidth=int(QtWidgets.QApplication.primaryScreen().size().width()/5.33)
@@ -17,6 +18,8 @@ conf=QSettings('Codec','codec')
 
 conf.value('animated')
 conf.value('spritesheet')
+conf.value('ssh_known_hosts')
+conf.value('ssh_authorized_keys')
 conf.sync
 
 class Subs(QtWidgets.QWidget):
@@ -69,6 +72,19 @@ class Codec(QtWidgets.QWidget):
         useract.triggered.connect(usertech.show)
         actiondd.addAction(useract)
 
+        sshcact=QtGui.QAction('Add an SSH connection',self)
+        sshcact.triggered.connect(sshctech.show)
+        actiondd.addAction(sshcact)
+
+        sshpact=QtGui.QAction('Check your SSH connections',self)
+        sshpact.triggered.connect(sshptech.show)
+        actiondd.addAction(sshpact)
+
+        quitact=QtGui.QAction('Quit',self)
+        quitact.triggered.connect(self.quitter)
+        actiondd.addAction(quitact)
+        quitact.setShortcut(Qt.Modifier.CTRL | Qt.Key.Key_Q)
+
         conf.beginGroup('Custom')
         if conf.value('usercmd1')!=None:
             self.customdd=self.menubar.addMenu('Custom')
@@ -91,6 +107,7 @@ class Codec(QtWidgets.QWidget):
         helphelp=QtGui.QAction('Help',self)
         helphelp.triggered.connect(self.helpdocs)
         helpdd.addAction(helphelp)
+        helphelp.setShortcut(Qt.Modifier.CTRL | Qt.Key.Key_H)
 
         abouthelp=QtGui.QAction('About',self)
         abouthelp.triggered.connect(abouttech.show)
@@ -138,6 +155,9 @@ class Codec(QtWidgets.QWidget):
             os.startfile('docs.md')
         else:
             subprocess.call(('xdg-open','docs.md'))
+
+    def quitter(self):
+        sys.exit(app.exec())
 
 class AboutInfo(QtWidgets.QWidget):
 
@@ -203,6 +223,277 @@ self.customdd.addAction({1})""".format(title,titlecleaned,cmd,cmdcleaned)
 usertech=UserCmd()
 usertech.setFixedSize(codecwidth*2,codecwidth-codecwidth/3)
 
+class SSHCAgent(QtWidgets.QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.layout=QtWidgets.QGridLayout(self)
+        self.quickbox=QtWidgets.QTextEdit(self)
+        self.quickbox.setPlaceholderText("Insert a ssh command 'ssh -p 2222 foo@bar.org'")
+        self.layout.addWidget(self.quickbox)
+        self.quickbox.setTabChangesFocus(True)
+        self.advanced=QtWidgets.QPushButton(self)
+        self.advanced.setText('Advanced')
+        self.advanced.clicked.connect(self.advancedmenu)
+        self.layout.addWidget(self.advanced)
+        self.hostaddress=QtWidgets.QPlainTextEdit(self)
+        self.hostaddress.setPlaceholderText('Enter the address or hostname')
+        self.layout.addWidget(self.hostaddress)
+        self.hostaddress.setTabChangesFocus(True)
+        self.hostaddress.hide()
+        self.username=QtWidgets.QPlainTextEdit(self)
+        self.username.setPlaceholderText('Enter the username')
+        self.layout.addWidget(self.username)
+        self.username.setTabChangesFocus(True)
+        self.username.hide()
+        self.hostport=QtWidgets.QPlainTextEdit(self)
+        self.hostport.setPlaceholderText('Enter the port')
+        self.layout.addWidget(self.hostport)
+        self.hostport.setTabChangesFocus(True)
+        self.hostport.hide()
+        self.password=QtWidgets.QPlainTextEdit(self)
+        self.password.setPlaceholderText('Enter the password')
+        self.layout.addWidget(self.password)
+        self.password.setTabChangesFocus(True)
+        self.password.hide()
+        self.keyButton=QtWidgets.QPushButton(self)
+        self.keyButton.clicked.connect(self.openFile)
+        self.keyButton.setText('Open a key file')
+        self.layout.addWidget(self.keyButton)
+        self.keyButton.hide()
+        self.key=None
+        self.connecter=QtWidgets.QPushButton(self)
+        self.connecter.setText('Connect')
+        self.connecter.clicked.connect(self.normalconnect)
+        self.layout.addWidget(self.connecter)
+
+    def openFile(self):
+        self.key=QtWidgets.QFileDialog()
+        self.key.setViewMode(QtWidgets.QFileDialog.ViewMode.List)
+        self.key=QtWidgets.QFileDialog.getOpenFileUrl().__str__()
+        clear=re.split("'",self.key)
+        clear.pop(0)
+        for x in range(3):
+            clear.pop(1)
+        self.key=''.join(clear)
+        clearer=re.split('//',self.key)
+        clearer.pop(0)
+        self.key=''.join(clearer)
+        if platform.system()=='Windows':
+            cleared=self.key.replace('/','C:\\',1)
+        else:
+            cleared=self.key
+
+    def normalmenu(self):
+        self.quickbox.show()
+        self.advanced.setText('Advanced')
+        self.advanced.clicked.connect(self.advancedmenu)
+        self.connecter.clicked.connect(self.normalconnect)
+        self.hostaddress.hide()
+        self.username.hide()
+        self.hostport.hide()
+        self.password.hide()
+        self.keyButton.hide()
+        sshctech.setFixedSize(codecwidth*1.15,codecwidth/2.65)
+
+    def advancedmenu(self):
+        self.quickbox.hide()
+        self.advanced.setText('Normal')
+        self.advanced.clicked.connect(self.normalmenu)
+        self.connecter.clicked.connect(self.advancedconnect)
+        self.hostaddress.show()
+        self.username.show()
+        self.hostport.show()
+        self.password.show()
+        self.keyButton.show()
+        sshctech.setFixedSize(codecwidth*1.15,codecwidth*0.90)
+
+    def normalconnect(self):
+        text=sshctech.quickbox.toPlainText()
+        os.system(text)
+
+    def advancedconnect(self):
+        address=self.hostaddress.toPlainText()
+        usernick=self.username.toPlainText()
+        passw=self.password.toPlainText()
+        portu=self.hostport.toPlainText()
+        keyf=self.key
+        ssh=paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            if portu=='' and keyf=='':
+                ssh.connect(hostname=address,username=usernick,password=passw,timeout=3)
+            elif keyf=='':
+                ssh.connect(hostname=address,username=usernick,password=passw,port=portu,timeout=3)
+            elif portu=='':
+                ssh.connect(hostname=address,username=usernick,password=passw,key_filename=keyf,timeout=3)
+            else:
+                ssh.connect(hostname=address,username=usernick,password=passw,port=portu,key_filename=keyf,timeout=3)
+            widgetdva.show()
+            widgetdva.subtext.setText("I'm in!")
+            widgetdva.subsdecay(2)
+            speechtech.speech()
+            ssh.save_host_keys(conf.value('known_hosts'))
+            ssh.close()
+        except paramiko.SSHException as error:
+            widgetdva.show()
+            widgetdva.subtext.setText(f"There seems to be an error... [{error}]")
+            widgetdva.subsdecay(4)
+            speechtech.speech()
+            ssh.close()
+            
+sshctech=SSHCAgent()
+sshctech.setFixedSize(codecwidth*1.15,codecwidth/2.65)
+
+class SSHPAgent(QtWidgets.QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.layout=QtWidgets.QGridLayout(self)
+        self.knownhostsButton=QtWidgets.QPushButton(self)
+        self.knownhostsButton.setText("Open 'known_hosts' file")
+        self.knownhostsButton.clicked.connect(self.openKHFile)
+        self.layout.addWidget(self.knownhostsButton)
+        self.authorizedkeysButton=QtWidgets.QPushButton(self)
+        self.authorizedkeysButton.setText("Open 'authorized_keys' file")
+        self.authorizedkeysButton.clicked.connect(self.openAKFile)
+        self.layout.addWidget(self.authorizedkeysButton)
+        self.namebox=QtWidgets.QTextEdit(self)
+        self.namebox.setPlaceholderText("Enter a username")
+        self.namebox.setTabChangesFocus(True)
+        self.layout.addWidget(self.namebox)
+        self.namebox.hide()
+        self.combox=QtWidgets.QTextEdit(self)
+        self.combox.setPlaceholderText("Enter a command to send")
+        self.combox.setTabChangesFocus(True)
+        self.layout.addWidget(self.combox)
+        self.combox.hide()
+        self.inputer=QtWidgets.QPushButton(self)
+        self.inputer.setText('Login')
+        self.layout.addWidget(self.inputer)
+        self.inputer.hide()
+        self.knownhostsValue=None
+        self.authorizedkeysValue=None
+        if conf.value('ssh_known_hosts') != '' and conf.value('ssh_authorized_keys') != '':
+            self.addUp()
+            self.knownhostsValue=conf.value('ssh_known_hosts')
+            self.authorizedkeysValue=conf.value('ssh_authorized_keys')
+
+    def openKHFile(self):
+        self.authorizedkeysValue=QtWidgets.QFileDialog()
+        self.authorizedkeysValue.setViewMode(QtWidgets.QFileDialog.ViewMode.List)
+        self.authorizedkeysValue=QtWidgets.QFileDialog.getOpenFileUrl().__str__()
+        clear=re.split("'",self.authorizedkeysValue)
+        clear.pop(0)
+        for x in range(3):
+            clear.pop(1)
+        self.authorizedkeysValue=''.join(clear)
+        clearer=re.split('//',self.authorizedkeysValue)
+        clearer.pop(0)
+        self.authorizedkeysValue=''.join(clearer)
+        if platform.system()=='Windows':
+            cleared=self.authorizedkeysValue.replace('/','C:\\',1)
+        else:
+            cleared=self.authorizedkeysValue
+        conf.setValue('ssh_known_hosts',self.authorizedkeysValue)
+        conf.sync
+        os.execv(sys.executable, ['Codec'] + sys.argv)
+
+    def openAKFile(self):
+        self.authorizedkeysValue=QtWidgets.QFileDialog()
+        self.authorizedkeysValue.setViewMode(QtWidgets.QFileDialog.ViewMode.List)
+        self.authorizedkeysValue=QtWidgets.QFileDialog.getOpenFileUrl().__str__()
+        clear=re.split("'",self.authorizedkeysValue)
+        clear.pop(0)
+        for x in range(3):
+            clear.pop(1)
+        self.authorizedkeysValue=''.join(clear)
+        clearer=re.split('//',self.authorizedkeysValue)
+        clearer.pop(0)
+        self.authorizedkeysValue=''.join(clearer)
+        if platform.system()=='Windows':
+            cleared=self.authorizedkeysValue.replace('/','C:\\',1)
+        else:
+            cleared=self.authorizedkeysValue
+        conf.setValue('ssh_authorized_keys',self.authorizedkeysValue)
+        conf.sync
+        os.execv(sys.executable, ['Codec'] + sys.argv)
+
+    def addUp(self):
+        ssh=paramiko.SSHClient()
+        ssh.load_host_keys(conf.value('ssh_known_hosts'))
+        x=0
+        authkeys=conf.value('ssh_authorized_keys')
+        for element in range(len(ssh._host_keys._entries)):
+            entryname=ssh._host_keys._entries[x].__str__()
+            clearname=re.split("'",entryname)
+            clearname.pop(0)
+            clearname.pop(1)
+            entryname=''.join(clearname)
+            entrypkey=ssh._host_keys._entries[x].__str__()
+            template="""style=self.style()
+online_icon=style.standardIcon(QStyle.StandardPixmap.SP_DialogYesButton)
+offline_icon=style.standardIcon(QStyle.StandardPixmap.SP_DialogNoButton)
+self.{0}=QtWidgets.QPushButton(self)
+check=os.system('ping -c 1 -W 5 {1}')
+if check==0:
+    self.{0}.setIcon(online_icon)
+else:
+    self.{0}.setIcon(offline_icon)
+self.{0}.setText("{1}")
+
+def loginpart(self):
+
+    def connectsequence(self):
+
+        usernick=sshptech.namebox.toPlainText()
+        usercom=sshptech.combox.toPlainText()
+        ssh=paramiko.SSHClient()
+        ssh.load_host_keys(conf.value('ssh_known_hosts'))
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh.connect(hostname='{1}',key_filename='{2}',username=usernick,timeout=500)
+            widgetdva.show()
+            widgetdva.subtext.setText("I'm in!")
+            widgetdva.subsdecay(2)
+            speechtech.speech()
+            stdin,stdout,stderr=ssh.exec_command(usercom)
+            output=stdout.read().decode('utf-8')
+            comerror=stderr.read().decode('utf-8')
+            if output:
+                widgetdva.subtext.setText(output)
+                widgetdva.subsdecay(2)
+                speechtech.speech()
+            if comerror:
+                widgetdva.subtext.setText('Error : '+comerror)
+                widgetdva.subsdecay(2)
+                speechtech.speech()
+            ssh.close()
+        except paramiko.SSHException as error:
+            widgetdva.show()
+            widgetdva.subtext.setText(f"There seems to be an error... ["+str(error)+" ]")
+            widgetdva.subsdecay(4)
+            speechtech.speech()
+            ssh.close()
+
+    sshptech.namebox.show()
+    sshptech.combox.show()
+    sshptech.inputer.show()
+    sshptech.inputer.clicked.connect(connectsequence)
+
+
+self.{0}.clicked.connect(loginpart)
+self.layout.addWidget(self.{0})""".format('connection'+str(x),entryname,authkeys)
+            code=compile(template,'<string>','exec')
+            exec(code)
+            x+=1
+
+sshptech=SSHPAgent()
+sshptech.setFixedWidth(codecwidth)
+
+
 class Blink(QtWidgets.QWidget):
 
     def blinker(self):
@@ -256,6 +547,7 @@ if widget.width > codecwidth:
     widget.setFixedSize(codecwidth,widget.height)
 else:
     widget.setFixedSize(widget.width,widget.height)
+widget.setWindowFlags(QtCore.Qt.WindowType.WindowStaysOnTopHint|QtCore.Qt.WindowType.FramelessWindowHint)
 widget.setWindowTitle('Codec')
 widget.show()
 sys.exit(app.exec())
--- setup.py
+++ setup.py
@@ -4,3 +4,5 @@ from PySide6.QtCore import QSettings
 conf=QSettings('Codec','codec')
 conf.setValue('animated','True')
 conf.setValue('spritesheet','otacondo.png')
+conf.setValue('ssh_known_hosts','')
+conf.setValue('ssh_authorized_keys','')
